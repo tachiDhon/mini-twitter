3216141c83c5546ec58fbdb4ecc286af873ebcae
Papi <emmanuel.kasomo@epitech.eu>
2019-07-08 11:22:06 +0200

    Tashi

--- script-tl.js
+++ script-tl.js
@@ -1,16 +1,33 @@
-$(function () {
-	$('[data-toggle="tooltip"]').tooltip()
-})
-
-$(function () {
-	$('[data-toggle="popover"]').popover()
-})
-
-function countChar(val) {
-	var len = val.value.length;
-	if (len >= 140) {
-		val.value = val.value.substring(0, 500);
-	} else {
-		$('#charNum').text(140 - len);
-	}
-};
+$('textarea').keyup(function() {
+    
+  var characterCount = $(this).val().length,
+      current = $('#current'),
+      maximum = $('#maximum'),
+      theCount = $('#the-count');
+    
+  current.text(characterCount);
+ 
+  
+  /*This isn't entirely necessary, just playin around*/
+  if (characterCount < 70) {
+    current.css('color', '#666');
+  }
+  if (characterCount > 70 && characterCount < 90) {
+    current.css('color', '#6d5555');
+  }
+  if (characterCount > 90 && characterCount < 100) {
+    current.css('color', '#793535');
+  }
+  if (characterCount > 100 && characterCount < 120) {
+    current.css('color', '#841c1c');
+  }
+  if (characterCount > 120 && characterCount <= 140) {
+    maximum.css('color', '#8f0001');
+    current.css('color', '#8f0001');
+    theCount.css('font-weight','bold');
+  } else {
+    maximum.css('color','#666');
+    theCount.css('font-weight','normal');
+  }
+       
+});
